6c2068df2859dfd861e907267c293a19e11642c2
Jeff Hutchinson <programmerjeff95@gmail.com>
2017-01-02 03:16:43 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection', function(socket) {
+	// Connected!
+	console.log("A client has connected!");
+
+	socket.on('message', (data) => {
+		io.emit('messageReceived', data);
+	});
+
+	socket.on('disconnect', () => {
+		console.log("A client has disconnected!");
+	});
+});
+
+http.listen(34122, () => {
+	console.log('Listening on port 34122');
+});
